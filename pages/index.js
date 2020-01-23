8711747a8d7d11e9fb2cd4ff7bccf03129c885b7
Rafael Cadena <rafaeljcadena@gmail.com>
2020-01-23 01:20:54 -0300

    testing fatures

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,11 @@ import Link from 'next/link';
 // import '../public/teste.css'
 import Button from '@material-ui/core/Button';
 import Head from 'next/head'
+import fetch from 'isomorphic-unfetch';
 
 // import '../styles/style.css'
 
-function getPosts() {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ];
-}
-
-function Index() {
+function Index({ shows }) {
   return (
     <Layout>
       <Head>
@@ -23,16 +16,14 @@ function Index() {
         <script type='application/ld+json'>{`{ "@context": "https://schema.org", "@type": "Organization", "url": "https://www.casadeapostasa.tv/", "sameAs": [], "@id": "https://www.casadeapostasa.tv/#organization", "name": "casa de apostas tv", "logo": "https://www.casadeapostas.tv/img/logo-white.png" }`}</script>
       </Head>
 
-      <h1>My Blog</h1>
       <Button variant="contained" color="secondary">Teste</Button>
+      <h1>Batman TV Shows</h1>
       <ul>
-        {getPosts().map(post => (
-          <li key={post.id}>
-            {/* <Link href="/posts/[id]" as={`/posts/${post.id}`}> */}
-              {/* <a className={style.teste}>{post.title}</a> */}
-              {/* <a className='teste'>{post.title}</a> */}
-            {/* </Link> */}
-            {post.title}
+        {shows.map(show => (
+          <li key={show.id}>
+            <Link href="/posts/[id]" as={`/posts/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
           </li>
         ))}
       </ul>
@@ -40,10 +31,15 @@ function Index() {
   );
 }
 
-Index.getInitialProps = async ctx => {
-  // const res = await fetch('https://api.github.com/repos/zeit/next.js')
-  // const json = await res.json()
-  return { stars: 'Foo' }
-}
+Index.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
 
 export default Index;
